55488ef0f26794a11b4abf422d4dbc6d6ffc78a0
UserPlayer1234 <theuserplayer1234@berkeley.edu>
2026-07-22 21:23:04 -0700

    backend: OAuth2 process

---
+++ authorize.py
@@ -0,0 +1,31 @@
+import os
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/forms.body']
+
+CLIENT_SECRET = "credentials.json"
+
+def obtain_creds() -> Credentials:
+    """Authorizes access to Google Forms by prompting Google login and returns credentials.
+
+        Returns:
+            Credentials: Returns existing credentials from token.json or creates new credentials from login
+    """
+
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRET, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
