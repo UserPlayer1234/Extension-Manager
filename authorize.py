df22c727417cc25a54a3548708e6c4bb3f673cee
UserPlayer1234 <theuserplayer1234@berkeley.edu>
2026-07-23 03:32:55 -0700

    backend: updated scope to allow reading responses

--- authorize.py
+++ authorize.py
@@ -4,7 +4,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-SCOPES = ['https://www.googleapis.com/auth/forms.body']
+SCOPES = ['https://www.googleapis.com/auth/forms.body', 'https://www.googleapis.com/auth/forms.responses.readonly']
 
 CLIENT_SECRET = "credentials.json"
 
