1676e021d799ac8b62b02f1dfab84008f18956f0
UserPlayer1234 <theuserplayer1234@berkeley.edu>
2026-07-26 22:00:18 -0700

    frontend: created functional UI for app

--- extensions_approver.py
+++ extensions_approver.py
@@ -13,15 +13,18 @@ def deny_extension():
     current_extension.approved = False
     st.session_state.current_extension = next(extensions_iter, extension_placeholder)
 
-st.header("Extensions Approver")
+def approver():
+    st.header("Extensions Approver")
 
-with st.container():
-    st.metric('Email', current_extension.email)
-    st.metric('Assignment', current_extension.assignment)
-    st.metric('Submitted On', current_extension.date_submitted)
-    st.metric('New Deadline', current_extension.deadline)
-    st.metric('Reason', current_extension.reason)
+    with st.container():
+        st.metric('Email', current_extension.email)
+        st.metric('Assignment', current_extension.assignment)
+        st.metric('Submitted On', current_extension.date_submitted)
+        st.metric('New Deadline', current_extension.deadline)
+        st.metric('Reason', current_extension.reason)
 
-with st.container(horizontal=True):
-    st.button('Approve', on_click=approve_extension)
-    st.button('Deny', on_click=deny_extension)
+    with st.container(horizontal=True):
+        st.button('Approve', on_click=approve_extension)
+        st.button('Deny', on_click=deny_extension)
+
+approver()
--- extensions_table.py
+++ extensions_table.py
@@ -5,30 +5,48 @@ from Extension import Extension
 
 extensions: dict[int, Extension] = st.session_state.extensions
 
-def change_handler():
-    changes = st.session_state["editor"]["edited_rows"]
+def retrieve_data() -> dict:
+    data = {
+        "Approved?": [ext.approved for ext in extensions.values()],
+        "Email": [ext.email for ext in extensions.values()],
+        "Assignment": [ext.assignment for ext in extensions.values()],
+        "Submitted On": [ext.date_submitted for ext in extensions.values()],
+        "New Deadline": [ext.deadline for ext in extensions.values()],
+        "Reason": [ext.reason for ext in extensions.values()],
+    }
+    return data
 
+def table():        
+    st.header("Extensions Table")
+
+    data = retrieve_data()
+
+    data = pd.DataFrame(data, index=list(extensions.keys()))
+
+    disabled_cols = ["_index", "Email", "Assignment", "Submitted On", "New Deadline", "Reason"]
+
+    column_config = {
+        "Approved?": st.column_config.CheckboxColumn("Approved?",pinned=True)
+    }
+
+    edited_data = st.data_editor(data, key="editor", width='stretch', disabled=disabled_cols, column_config=column_config)
+
+def update_extensions():
+    changes = st.session_state["editor"]["edited_rows"]
+    
     # Extract new boolean values
     toggled = {row: cols["Approved?"] for row, cols in changes.items() if "Approved?" in cols}
 
     # Update Extensions
     for row, new_val in toggled.items():
         extension = next((extension for extension in extensions.values() if extension.id == row), None)
-        extension.approved = new_val
-        
-st.header("Extensions Table")
+        if extension:
+            extension.approved = new_val
 
-data = {
-    "Email": [ext.email for ext in extensions.values()],
-    "Assignment": [ext.assignment for ext in extensions.values()],
-    "Submitted On": [ext.date_submitted for ext in extensions.values()],
-    "New Deadline": [ext.deadline for ext in extensions.values()],
-    "Reason": [ext.reason for ext in extensions.values()],
-    "Approved?": [ext.approved for ext in extensions.values()],
-}
+    print(extensions)
 
-data = pd.DataFrame(data, index=list(extensions.keys()))
+def update_button():
+    update = st.button('Save Changes', on_click=update_extensions)
 
-disabled_cols = ["_index", "Email", "Assignment", "Submitted On", "New Deadline", "Reason"]
-
-edited_data = st.data_editor(data, key="editor", width='stretch', disabled=disabled_cols, on_change=change_handler)
+table()
+update_button()
