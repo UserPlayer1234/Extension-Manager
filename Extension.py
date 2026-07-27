b62db9b9bd8c8b0c69dc6ab96b4678b2944f6775
UserPlayer1234 <theuserplayer1234@berkeley.edu>
2026-07-26 21:59:41 -0700

    backend: made empty Extension default to id = -1

--- Extension.py
+++ Extension.py
@@ -12,10 +12,10 @@ class Extension:
         self.reason: str = reason
         self.date_submitted: str = date_submitted
         self.approved: bool = False
-        self.id: int = next(Extension.counter)
+        self.id: int = next(Extension.counter) if email != 'N/A' else -1
 
     def __repr__(self):
-        return f"{self.id} : {self.email} | {self.assignment} | {self.reason}"
+        return f"{self.id} : {self.approved}"
 
     def __eq__(self, value):
         if isinstance(value, Extension) and value.id == self.id:
